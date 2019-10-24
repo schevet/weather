c540b04a9eee2e70a9360784b6da0c7944a4c7e5
schevet <scheveuni@gmail.com>
2019-10-24 12:37:59 -0500

    tried adding parameters

--- java.js
+++ java.js
@@ -2,43 +2,47 @@ $(document).ready(function () {
     // console.log($("#searchTermLabel").val());
 
     $(".search").on("click", function (event) {
+
         event.preventDefault()
+
         //  alert("clicked");
         var authKey = "&appid=b2fd13cdd07e8064f83f4e3c1f55ea30";
-        var currentWeather = "api.openweathermap.org/data/2.5/weather?q=";
 
+        var currentWeather = "api.openweathermap.org/data/2.5/weather?q=";
+        var forecast = "api.openweathermap.org/data/2.5/forecast?q=";
 
         var city = $("#citySearch").val();
 
-        currentWeather += city;
+        for (var i = 0; i < city.length; i++) {
+            if (city.charAt(i) === " ") {
+                var city = city.replace(" ", "%20");
+            };
+        };
 
+        currentWeather += city;
         currentWeather += authKey;
 
-        console.log(currentWeather);
+        forecast += city;
+        forecast += authKey;
 
-        // $.ajax({
-        //   url: queryURLBase,
-        //   method: "GET"
-        // }).then(function(data) {
-        //   console.log(api.openweathermap.org/data/2.5/forecast?q={city name},{country code});
-        //get all the data you need from the data.response.docs[0]. once that is done
-        //url, title, descrption (whatever instruct says)
-        //dynamically create a div, append that data to the div, then append the data to the html doc
-        //to dynamically append review the fridge game
-        //change that to a forloop to get it for the rest
+        console.log("temperature: ", currentWeather.main);
+        console.log("humidity: ", currentWeather);
+        console.log("windspeed: ", currentWeather.wind);
+        console.log(forecast);
 
-        //record count
-        //if the record count is > than the results, use the results length
-        /* if(recordcout>data.reponse.docs.length){
-           recordcount=data.resonse.doc.length;
-         }*/
+        // ajax for current weather
+        $.ajax({
+            url: currentWeather,
+            method: "GET"
+        }).then(function (url) {
+            console.log(url);
+        });
 
-        // for (var i = 0; i < recordCount; i++) {
-        //     console.log(results)
-        //     var articleDiv = $("<div>");
-
-        // console.log(searchURL);
-        // };
+        $.ajax({
+            url: forecast,
+            method: "GET"
+        }).then(function (url) {
+            console.log(url);
+        });
     });
-
 });
